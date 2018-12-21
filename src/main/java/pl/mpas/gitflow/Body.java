2830746c70946f348fc20da1ddbb4b6e923090b0
maniek <mpastuszka@gmail.com>
2018-12-21 15:00:57 +0100

    [change] - adding some scenario information

--- src/main/java/pl/mpas/gitflow/Body.java
+++ src/main/java/pl/mpas/gitflow/Body.java
@@ -6,3 +6,25 @@ public class Body {
     String a;
     String h2;
 }
+// tworzymy zdalne repo: git init --bare new.git
+// dzielimy się linkiem
+// c:/program files/repo
+//git clone adres_repo
+
+// git remote -v
+
+// git checkout -b develop
+// git branch -a
+
+// TODO
+// feature branch imie-nazwisko
+// git branch -b develop
+
+// wspolny.txt
+// imie-nazwisko.txt
+
+//git add wspolny.txt imie-nazwisko.txt
+// git commit -m "[init] - initial commit"
+
+// powrót na development
+// git checkout development
